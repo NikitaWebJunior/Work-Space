6cd3519b93b86648aecfb4493cc866008f4fbabf
Nikita Khveshchenko <nikitakhveshchenko@mail.ru>
2024-05-22 10:33:35 +0300

    feat: add get and render locations using API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
 const citySelect = document.querySelector('#city');
 
 const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
   itemSelectText: "",
 });
+
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    cbSuccess(data);
+  } 
+  catch (error) {
+    cbError(error);
+  }
+}
+
+const getAndRenderLocations = getData(API_URL + LOCATION_URL, 
+  (data) => {
+     const locations = data.map((location) => ({
+      value: location
+    }))
+    cityChoices.setChoices(locations, "value", "label", true)
+  }, 
+  (error) => console.log(error));
+
+
